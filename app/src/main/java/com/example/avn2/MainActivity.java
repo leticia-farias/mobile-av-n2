b2d024342f853ce0b69ce0f187271117c048b1c5
leticia-farias <leticiafarias.engsoftware@gmail.com>
2025-11-29 17:29:07 -0300

    First commit

---
+++ app/src/main/java/com/example/avn2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.avn2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mapButton = findViewById(R.id.map_button);
+        mapButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.map_button) {
+//            Intent i = new Intent(MainActivity.this, MapsActivity.class);
+//            startActivity(i);
+        }
+    }
+}
